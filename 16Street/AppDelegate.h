faad0aaeda8c8833d17e96ef0f860f3629eff4e7
程超 <76569478@qq.com>
2018-10-10 17:27:16 +0800

    Initial Commit

---
+++ 16Street/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  16Street
+//
+//  Created by apple on 2018/10/10.
+//  Copyright © 2018年 toerax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 16Street/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  16Street
+//
+//  Created by apple on 2018/10/10.
+//  Copyright © 2018年 toerax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
